7c849c8cc47d94fbb8f15241db5ac148fab95adb
Hasanhamdan789 <HasanHamdan789@gmail.com>
2023-07-17 14:15:07 +0300

    needs revising

--- pythonreview.py
+++ pythonreview.py
@@ -1,15 +1,35 @@
-def create_youtube_video("title", "description"):
-	youtubeVideo = {"title": title, "description": description, "likes":0, "dislikes":0, "comments":{}}
-	return youtubeVideo
+def create_youtube_video(title, description):
+  youtubeVideo = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+  return youtubeVideo
 
 def like(youtubeVideo):
-	youtubeVideo["likes"] +=1
-	return youtubeVideo
+  if "likes" in youtubeVideo:
+    youtubeVideo["likes"] += 1
+  return youtubeVideo
 
 def dislike(youtubeVideo):
-	youtubeVideo["dislikes"] +=1
-	return youtubeVideo
+  if "dislikes" in youtubeVideo:
+    youtubeVideo["dislikes"] += 1
+  return youtubeVideo
 
-def add_comment():
-	 
+def add_comment(youtubeVideo, username, comment_text):
+  if "comments" not in youtubeVideo:
+    youtubeVideo["comments"] = {}
+  youtubeVideo["comments"][username] = comment_text
+  return youtubeVideo
 
+youtubeVideo = create_youtube_video("My First Video", "This is my first video on YouTube.")
+
+print(youtubeVideo)
+
+youtubeVideo = like(youtubeVideo)
+print(youtubeVideo)
+
+youtubeVideo = dislike(youtubeVideo)
+print(youtubeVideo)
+
+youtubeVideo = add_comment(youtubeVideo, "Hellooo!", "This is a great video!")
+print(youtubeVideo)
+
+youtubeVideo = int(youtubeVideo["likes"]) + 494
+print(youtubeVideo)
